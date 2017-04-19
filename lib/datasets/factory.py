e059b2a4879254c1ab3e17fa25a6b47c98d47a0d
w4wilk <w4wilk@gmail.com>
2017-04-19 12:00:06 +0200

    Added HANDS dataset placeholders. Hands.py and train/test.prototxt are still using 21 class voc dataset.

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -11,6 +11,7 @@ __sets = {}
 
 from datasets.pascal_voc import pascal_voc
 from datasets.coco import coco
+from datasets.hands import hands
 import numpy as np
 
 # Set up voc_<year>_<split> using selective search "fast" mode
@@ -30,6 +31,11 @@ for year in ['2015']:
     for split in ['test', 'test-dev']:
         name = 'coco_{}_{}'.format(year, split)
         __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+# Set up hands_<split>
+for split in ['train', 'test']:
+    name = '{}_{}'.format('hands', split)
+    __sets[name] = (lambda split=split: hands(split, 'HANDSdevkit'))
 
 def get_imdb(name):
     """Get an imdb (image database) by name."""
